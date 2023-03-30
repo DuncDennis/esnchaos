c6a18910fe693a9597487435dbf1df0361276f6f
DuncDennis <90915296+DuncDennis@users.noreply.github.com>
2023-03-30 15:42:50 +0200

    Added plot_results file 07_appendix.

---
+++ mt_plots/ensemble_experiment_plots/07_appendix/RRtypeAndValidtimes/plot_results.py
@@ -0,0 +1,67 @@
+"""Read all pkl files from a designated folder and save the plots.
+TODO: For now does not handle the saving of the plots -> has to be implemented.
+"""
+
+from pathlib import Path
+import plotly.io as pio
+
+import esnchaos.plot.pkl_to_df as pkl_to_df
+import esnchaos.plot.df_to_plot as df_to_plot
+import esnchaos.plot.utilities as plot_utils
+
+col_pal_simple_white = pio.templates["simple_white"].layout.colorway
+
+RESULTS_PKL_DIR = "original_data"
+Y_METRIC = "M VALIDATE VT_centered"
+X_PARAM = "P data_offset"
+COL_PARAM = "P rr_type"
+AVG_MODE = "median_and_quartile"
+
+Y_AXIS_DICT = dict(
+    dtick=2
+)
+
+X_AXIS_DICT = dict(
+)
+
+GRID_SETTINGS = dict(
+    showgrid=True,
+    gridwidth=1,
+    gridcolor="gray"
+)
+
+if __name__ == "__main__":
+    path = Path(RESULTS_PKL_DIR)
+
+    pkl_files = list(path.glob("*.pkl"))
+
+    for i, pkl_file_path in enumerate(pkl_files):
+
+        df = pkl_to_df.read_pkl(pkl_file_path)
+
+        df_pre_fil = pkl_to_df.pre_filter_df(df, rmv_const_cols=True)
+
+        df_agg = pkl_to_df.aggregate_df(df_pre_fil,
+                                        avg_mode=AVG_MODE)
+
+        # global params:
+        params = dict(
+            y_axis_dict=Y_AXIS_DICT,
+            x_axis_dict=X_AXIS_DICT,
+            width=700,
+            height=500,
+            line_width=3,
+            error_thickness=2,
+            color_alpha=1.0,
+            y_grid_settings_dict=GRID_SETTINGS,
+            metric_transform_ltx={
+                "M VALIDATE VT_centered": r" t_\text{v} \lambda_\text{max}$"}
+        )
+
+        fig = df_to_plot.plot_two_dim_sweep(df_agg,
+                                            x_param=X_PARAM,
+                                            y_metric=Y_METRIC,
+                                            col_param=COL_PARAM,
+                                            params=params)
+
+        fig.write_image(f"test.png", scale=3)
